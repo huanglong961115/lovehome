900b03cc17b297e20db0b8da82927b1b7f1a33c2
huanglong961115 <1850831225@qq.com>
2016-07-18 21:02:48 +0800

    我的资料

--- app/src/main/java/com/example/lovehometown/activity/UserInfoActivity.java
+++ app/src/main/java/com/example/lovehometown/activity/UserInfoActivity.java
@@ -2,17 +2,33 @@ package com.example.lovehometown.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.lovehometown.R;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 
 @ContentView(R.layout.activity_user_info)
 public class UserInfoActivity extends BaseActivity {
-
+    @ViewInject(R.id.title)
+    TextView title;
+    @ViewInject(R.id.leftView)
+    ImageView backImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initView();
+    }
+    /**
+     * 初始化视图
+     */
+    public void initView(){
+        title.setVisibility(View.VISIBLE);
+        title.setText("用户资料");
+        backImg.setVisibility(View.VISIBLE);
 
     }
 }
