ee050aef5e07896b0946130f6381f3856b01ea79
lztyytzl <1156258101@qq.com>
2016-07-18 16:49:23 +0800

    注册

--- app/src/main/java/com/example/lovehometown/activity/RegisterActivity.java
+++ app/src/main/java/com/example/lovehometown/activity/RegisterActivity.java
@@ -13,6 +13,5 @@ public class RegisterActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 }
