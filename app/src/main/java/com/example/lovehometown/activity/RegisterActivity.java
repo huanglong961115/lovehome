bb6e01355c19c8d1f8dd559c525e143d0b624931
lztyytzl <1156258101@qq.com>
2016-07-18 16:54:33 +0800

    注册2

--- app/src/main/java/com/example/lovehometown/activity/RegisterActivity.java
+++ app/src/main/java/com/example/lovehometown/activity/RegisterActivity.java
@@ -7,11 +7,13 @@ import com.example.lovehometown.R;
 
 import org.xutils.view.annotation.ContentView;
 
+/*注册*/
 @ContentView(R.layout.activity_register)
 public class RegisterActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 }
