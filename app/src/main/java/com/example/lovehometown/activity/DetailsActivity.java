32d5cfb314be8c93e1399e6850eca0fd254a58c5
huanglong961115 <1850831225@qq.com>
2016-07-18 19:09:16 +0800

    我的2

---
+++ app/src/main/java/com/example/lovehometown/activity/DetailsActivity.java
@@ -0,0 +1,40 @@
+package com.example.lovehometown.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.lovehometown.R;
+import com.example.lovehometown.constant.Constants;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+@ContentView(R.layout.activity_details)
+public class DetailsActivity extends BaseActivity {
+    @ViewInject(R.id.details_webview)
+    WebView webview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webview.loadUrl(Constants.DETAILS_URL);
+        webview.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // TODO Auto-generated method stub
+                view.loadUrl(url);
+                return true;
+            }
+
+        });
+
+        WebSettings webSettings = webview.getSettings();
+        //设置支持javascript
+        webSettings.setJavaScriptEnabled(true);
+        //设置无缓存
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+    }
+}
--- app/src/main/java/com/example/lovehometown/constant/Constants.java
+++ app/src/main/java/com/example/lovehometown/constant/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
     //存储是否第一次启动应用程序
     public final static String IS_FIRST="isFirst";
     //首页图片轮播地址
+<<<<<<< Updated upstream
     public final static String IMG_WEB_URL="http://172.16.46.39:8080/LoveHome/index.html";
     //平台声明地址
     public final static String  PLATFORM_STATEMENT_URL="http://172.16.46.39:8080/LoveHome/PlatformStatement.html";
@@ -16,4 +17,9 @@ public class Constants {
     public final static String  ABOUT_LOVE_HOME_URL="http://172.16.46.39:8080/LoveHome/PlatformStatement.html";
     //是否登录在share存放的key值
     public final static String IS_LOGIN="islogin";
+=======
+    public final static String IMG_WEB_URL="http://172.16.46.91:8080/LoveHome/index.html";
+    //详情的地址
+    public final static String DETAILS_URL="http://172.16.46.65:8020/LoveHomeTown/details.html";
+>>>>>>> Stashed changes
 }
