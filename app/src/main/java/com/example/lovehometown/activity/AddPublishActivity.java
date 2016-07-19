ec93f14989b3aab926bbc481827ea6a7fcd229e6
huanglong961115 <1850831225@qq.com>
2016-07-19 10:28:45 +0800

    添加发布

---
+++ app/src/main/java/com/example/lovehometown/activity/AddPublishActivity.java
@@ -0,0 +1,33 @@
+package com.example.lovehometown.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.lovehometown.R;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+@ContentView(R.layout.activity_add_publish)
+public class AddPublishActivity extends BaseActivity {
+    /*标题栏*/
+    @ViewInject(R.id.leftView)
+    private ImageView img;
+    @ViewInject(R.id.title)
+    private TextView title;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        initView();
+    }
+    private void initView() {
+        /*设置可见*/
+        img.setVisibility(View.VISIBLE);
+        title.setVisibility(View.VISIBLE);
+
+    }
+}
